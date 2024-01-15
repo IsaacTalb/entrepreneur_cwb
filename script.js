f0fcd29c6431021a5c24b77812cb2739da3e8053
Isaac_Talb <thaungahlaybaw@gmail.com>
2024-01-15 14:48:28 +0630

    homepage ideas started

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ function changeBodySection(section) {
     document.getElementById('phoomyat').classList.add('hidden');
     document.getElementById('minishin').classList.add('hidden');
     document.getElementById('t-home').classList.add('hidden');
-
+    document.getElementById('hidden_homepage').classList.add('hidden');
+    
+    
     // Show the selected section
-    document.getElementById(section).classList.remove('hidden');}
+    document.getElementById(section).classList.remove('hidden');
+}
+
+/*
+document.getElementById('yourBodySectionId').classList.add('hidden'); hides the body section
+ with the ID 'yourBodySectionId'. Make sure to replace 'yourBodySectionId' with the actual ID of
+  the body section you want to hide.
+
+If you also want to show the body section again under
+certain conditions, you can add a similar line to remove the 'hidden' class:
+
+document.getElementById('yourBodySectionId').classList.remove('hidden');
+*/
+
+
+function changeActive(element) {
+    // Remove 'active' class from all links
+    document.querySelectorAll('nav a').forEach(function(link) {
+      link.classList.remove('active');
+       });
+
+    // Add 'active' class to the clicked link
+    element.classList.add('active');}
+
+
+
